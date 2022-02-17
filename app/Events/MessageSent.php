d02bb0cffae0b67aef02121d7dbf84856f1f1fca
Wunna <wana192770@gmail.com>
2022-02-17 21:34:14 +0630

    complete

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\User;
+use App\Models\Message;
+
+class MessageSent implements ShouldBroadcast
+{
+    public $user;
+    public $message;    
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Message $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ resources/js/components/ChatForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="input-group">
+        <input type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
+        <!-- @keyup.enter  ==> Enter key in keyboard -->
+
+        <span class="input-group-btn">
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">Send</button>
+        </span>
+    </div>
+</template>
+
+
+<script>
+export default {
+    //Takes the "user" props from <chat-form> … :user="{{ Auth::user() }}"></chat-form> in the parent chat.blade.php.
+    props: ['user'],
+    data () {
+        return {
+            newMessage: "", 
+        };
+    },
+
+    methods: {
+        sendMessage() {
+            this.$emit("messagesent", {
+                user: this.user,    
+                message: this.newMessage,
+            });
+            // clear the input
+            this.newMessage = "";
+        },
+    },
+}
+</script>
